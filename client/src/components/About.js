c496644aec1b18b0682af6dc8f8daf81e944ae1b
angpace <108838753+angpace@users.noreply.github.com>
2023-05-19 17:34:12 -0400

    Components folder, parallax scroll package and minimal implementation.

---
+++ client/src/components/About.js
@@ -0,0 +1,17 @@
+import { Parallax } from "react-scroll-parallax"
+
+const About = () => {
+// const { ref } = useParallax({speed:10});
+
+    return(
+        <div >
+            <h1>About us</h1>
+            <p>Foglia started like many other businesses, during the pandemic. Angela Pace, a
+                former Pastry Chef at Eleven Madison Park, continously made kombucha throughout the pandemic when
+                the light bulb went off. Foglia started with a 14 bottle-batch of kombucha out of a Sunnyside Apartment. 
+            </p>
+        </div>
+    )
+}
+
+export default About;

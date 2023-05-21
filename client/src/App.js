33130c0431c756fed116d722ffe96eeb0c6cc402
angpace <108838753+angpace@users.noreply.github.com>
2023-05-21 10:27:34 -0400

    Adding navbar and sidebar elements. Styled components, react scroll and react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,50 +1,11 @@
 import { BrowserRouter as Router } from 'react-router-dom'
-import Navbar from './components/NavBar';
+import Home from './pages';
 
 
 function App() {
-
-
-
   return (
-  //   <ParallaxProvider>
-  //     <nav id="navbar-example2" class="navbar fixed-bottom bg-light">
-  //       <div className='container-fluid'>
-  //   <a class="navbar-brand" href="#landing">Foglia</a>
-  //   <ul class="nav nav-pills">
-  //     <li class="nav-item">
-  //       <a class="nav-link" href="#about">About</a>
-  //     </li>
-  //     <li class="nav-item">
-  //       <a class="nav-link" href="#contact">Contact</a>
-  //     </li>
-  //     <li class="nav-item">
-  //       <a class="nav-link" href="#shop">Shop</a>
-  //     </li>
-  //   </ul>
-  //   </div>
-  // </nav>
-  // <div className='top-banner-text'><span className='top-banner'></span></div>
-  //   <div className="parallax-container">
-  //     <div className="parallax-layer"  >
-  //       <Parallax y={[-20, 20]} tagOuter="div" id="landing">
-  //         <Landing/>
-  //       </Parallax>
-  //     </div>
-  //     <div className="parallax-layer">
-  //       <Parallax y={[-30, 30]} tagOuter="div" id="about">
-  //         <About/>
-  //       </Parallax>
-  //     </div>
-  //     <div className="parallax-layer" id="contact">
-  //       <Parallax y={[-40, 40]} tagOuter="div">
-  //         Layer 3
-  //       </Parallax>
-  //     </div>
-  //   </div>
-  // </ParallaxProvider>
   <Router>
-   <Navbar/>
+   <Home/>
   </Router>
   );
 }

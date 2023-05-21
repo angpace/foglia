342fc72a976e17bcf1d69e079cccb3f7d426ad53
angpace <108838753+angpace@users.noreply.github.com>
2023-05-21 18:55:21 -0400

    Addinghero section and reusable button

--- client/src/components/HeroSection/index.js
+++ client/src/components/HeroSection/index.js
@@ -1,13 +1,28 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Video from '../../videos/video.mp4'
-import { HeroContainer, HeroBg, VideoBg } from './HeroElements'
+import { Button } from '../ButtonElement'
+import { HeroContainer, HeroBg, VideoBg, HeroContent, HeroH1, HeroP, HeroBtnWrapper, ArrowForward, ArrowRight} from './HeroElements'
 
 const HeroSection = () => {
+  const [hover, setHover] = useState(false)
+
+  const onHover = () => {
+    setHover(!hover)
+  }
+
   return (
     <HeroContainer id="home">
         <HeroBg>
             <VideoBg autoPlay loop muted src={Video} type='video/mp4' />
         </HeroBg>
+        <HeroContent>
+          <HeroH1>Welcome to Foglia</HeroH1>
+          <HeroP>Redefine Bakery.</HeroP>
+          <HeroBtnWrapper>
+            <Button to="shop" onMouseEnter={onHover} onMouseLeave={onHover}
+            >Check it out. {hover? <ArrowForward /> : <ArrowRight />}</Button>
+          </HeroBtnWrapper>
+        </HeroContent>
     </HeroContainer>
   )
 }

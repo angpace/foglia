908ad9d7c89429b59b6c7893a93647d639a5d863
angpace <108838753+angpace@users.noreply.github.com>
2023-05-22 12:37:24 -0400

    Adding first Info Section

--- client/src/components/InfoSection/index.js
+++ client/src/components/InfoSection/index.js
@@ -1,8 +1,34 @@
 import React from 'react'
+import { Button } from 'react-scroll'
+import { Column2, ImgWrap, InfoContainer, InfoWrapper, InfoRow, Column1, TextWrapper, TopLine, Heading, Subtitle, BtnWrap, Img } from './InfoElements'
 
 const InfoSection = () => {
   return (
-    <div>InfoSection</div>
+    <>
+    <InfoContainer>
+      <InfoWrapper>
+        <InfoRow>
+          <Column1>
+          <TextWrapper>
+            <TopLine>
+              TopLine
+            </TopLine>
+            <Heading>heading</Heading>
+            <Subtitle>Subtitle</Subtitle>
+            <BtnWrap>
+              <Button to="about"></Button>
+            </BtnWrap>
+          </TextWrapper>
+          </Column1>
+          <Column2>
+          <ImgWrap>
+            <Img/>
+          </ImgWrap>
+          </Column2>
+        </InfoRow>
+      </InfoWrapper>
+    </InfoContainer>
+    </>
   )
 }
 

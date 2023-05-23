bf293898c88db1d683bcf7481d112368eb45024d
angpace <108838753+angpace@users.noreply.github.com>
2023-05-23 12:41:07 -0400

    Minor changes to see the difference in Data in info section.

--- client/src/components/InfoSection/Data.js
+++ client/src/components/InfoSection/Data.js
@@ -5,8 +5,8 @@ export const homeObjOne = {
     lightText: false,
     lightTextDesc: true,
     topLine: 'On our way!',
-    headline: 'Small batch Kombucha.',
-    description: 'A mix of green and black tea, fresh fruit and thousands of probitics, shipping now.',
+    headline: 'Delivery Now Available.',
+    description: 'Started in our home in Sunnyside, Queens, shipping to wherever you call home.',
 
     buttonLabel: 'Take a Look',
     imgStart: true,
@@ -19,24 +19,24 @@ export const homeObjOne = {
 
 export const homeObjTwo = {
     id: 'shop',
-    lightBg: false,
-    lightText: false,
-    lightTextDesc: true,
-    topLine: 'On our way!',
-    headline: 'Small batch Kombucha.',
-    description: 'A mix of green and black tea, fresh fruit and thousands of probitics, shipping now.',
+    lightBg: true,
+    lightText: true,
+    lightTextDesc: false,
+    topLine: 'About Us',
+    headline: 'Delivery Now Available.',
+    description: 'Started in Sunnyside, Queens, shipping to wherever.',
 
-    buttonLabel: 'Take a Look',
+    buttonLabel: 'Learn More',
     imgStart: true,
     img: require('../../images/svg-4.svg').default,
     alt: 'kombucha',
     dark: true,
-    primary: true,
+    primary: false,
     darkText: false
 }
 
 export const homeObjThree = {
-    id: 'signin',
+    id: 'signup',
     lightBg: false,
     lightText: false,
     lightTextDesc: true,
